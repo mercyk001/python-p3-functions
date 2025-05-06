869963e5c1497cf54534b49d8e9ace7e6c2d07d8
mercy chepkemoi koech <koechmercyk@gmail.com>
2025-05-06 22:01:51 +0300

    functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    #pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
-
+    #pass
+    print(f"Hello, {name}!")
+    
 def greet_with_default(name="programmer"):
-    pass
+    #pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    #pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    #pass
+    return number / 2
+
